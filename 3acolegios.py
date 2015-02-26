73bc0f8e2d711460caea029a2abdfcb7187dbedf
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-26 21:37:26 +0100

    Terminando el Ejercicio3

--- 3acolegios.py
+++ 3acolegios.py
@@ -3,3 +3,6 @@ from lxml import etree
 arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
 directorios=arbol.getroot()
 direct=directorios.findall("directorio")
+for cole in direct:
+	if "Comedor: S" in cole.find("descripcion").text:
+			print cole.find("nombre").text
