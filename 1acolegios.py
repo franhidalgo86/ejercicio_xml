873b4589cf383a670833c59c4db564b176a39e55
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-26 20:28:21 +0100

    primer ejercicio de xlm

--- 1acolegios.py
+++ 1acolegios.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
+arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
+directorios=arbol.getroot()
 direct=directorios.findall("directorio/nombre")
 for nom in direct:
 	print nom.text
