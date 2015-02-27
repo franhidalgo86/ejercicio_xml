f4606ab9d401a4349bfad449824d7e9f3c30b432
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:17:33 +0100

    Ejercicio 5 terminado

--- 5acolegios.py
+++ 5acolegios.py
@@ -2,4 +2,10 @@
 from lxml import etree
 arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
 directorios=arbol.getroot()
-ident=directorios.findall("directorio")
+ident=directorios.findall("directorio/identificador")
+for ide in ident:
+	print ide.text
+idcole=raw_input("Mete algun ID para saber el Colegio que es: ")
+for iden in directorios:
+	if iden.find("identificador").text==idcole:
+		print iden.find("nombre").text
