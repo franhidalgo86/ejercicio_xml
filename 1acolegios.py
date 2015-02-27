0cb44359fb90c2d11b4952ca7aca6e87c8a97f9c
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:04:28 +0100

    Ejercicio 1 terminado

--- 1acolegios.py
+++ 1acolegios.py
@@ -4,4 +4,4 @@ arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
 directorios=arbol.getroot()
 direct=directorios.findall("directorio/nombre")
 for nom in direct:
-	print nom.text
+	print nom.text 
