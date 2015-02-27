9a081cbd8d7aab9ea7a13216cc2fd0ece19622cf
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:23:20 +0100

    Ejercicio 4 modificado

--- 4acolegios.py
+++ 4acolegios.py
@@ -3,4 +3,5 @@ from lxml import etree
 arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
 directorios=arbol.getroot()
 direct=directorios.findall("directorio")
-
+contador=0
+for astu in direct:
