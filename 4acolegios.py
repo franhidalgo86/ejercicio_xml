aaa76e8dc020c20ba68bdfa5a6abee90584893e1
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:20:41 +0100

    Ejercicio 4 comienzo

---
+++ 4acolegios.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
+directorios=arbol.getroot()
+direct=directorios.findall("directorio")
+
