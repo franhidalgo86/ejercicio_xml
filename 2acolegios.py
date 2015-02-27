174bc95298672a32709976f07c56cd718a4548d7
Fran Hidalgo <franhidalgo86@hotmail.com>
2015-02-27 14:30:02 +0100

    Ejercicio 2 modificando

--- 2acolegios.py
+++ 2acolegios.py
@@ -1,4 +1,8 @@
-# -*- coding: utf-8 -*-
+# _*_ coding: utf-8 _*_
 from lxml import etree
 arbol=etree.parse("/home/franhidalgo/Documentos/LM/XML/colegios.xml")
 directorios=arbol.getroot()
+direct=directorios.findall("directorio/nombre")
+for nom in direct:
+	print nom.text
+cole=raw_input("Mete un colegio: ")
